c053a5bc8d260c2a7123312903a3b693f64ba36f
zmocheng <zmocheng@gmail.com>
2022-05-20 17:59:18 +0200

    Format files.

--- Triangulation/triangulation_method.cpp
+++ Triangulation/triangulation_method.cpp
@@ -99,7 +99,7 @@ namespace GEO1016_A2 {
     * avg = dc/N
     * in order to make the average distance of a point p from the origin is equal to ¡Ì2:
     * avg * s = ¡Ì2, thus s = ¡Ì2/avg, s is the scaling factor
-    * 
+    *
     * Then the transform matrix for one image is:
     *     s    0   -stx
     * T = 0    s   -sty     -> T is a 3 by 3 matrix
@@ -109,14 +109,14 @@ namespace GEO1016_A2 {
     * std::pair<Matrix33, bool>
     * first element is the constructed transform matrix
     * second element is a bool variable indicating whether the matrix is susseccfully constructed
-    */  
+    */
     std::pair<Matrix33, bool> getNormalizeTransformMatrix(
         const std::vector<Vector2D>& points)
     {
         // elements will be returned
         Matrix33 T;
         bool T_valid = false;
-        
+
         // transform matrix --------------------------------------------------------------
         double sumX = 0;  // for calculating image's center
         double sumY = 0;
@@ -132,11 +132,11 @@ namespace GEO1016_A2 {
             LOG(ERROR) << "please check the divisor for x and y coordinates\n";
             return std::make_pair(T, T_valid);  // T_valid remains false, will not trigger further process
         }
-        
+
         // image center: (tx, ty)
         double tx = sumX / N;
-        double ty = sumY / N; 
-        
+        double ty = sumY / N;
+
         // scale factor
         double dc_squared = 0;
         for (const auto& p : points)
@@ -164,10 +164,10 @@ namespace GEO1016_A2 {
     /*
     * NormalizePoints
     * apply transform matrix to 2D points
-    * 
+    *
     * @param:
     * points need to be normalized, transform matrix M
-    * 
+    *
     * @return:
     * std::pair<std::vector<Vector2D>, bool>
     * first element is the normalized points set
@@ -179,7 +179,7 @@ namespace GEO1016_A2 {
         // elements will be returned
         std::vector<Vector2D> np;
         np.reserve(points.size());
-        
+
         // normalize points
         for (const auto& p : points)
         {
@@ -207,31 +207,31 @@ namespace GEO1016_A2 {
         Matrix33 F;
         bool F_valid = false;
         // elements will be returned: Fundamental Matrix, its status
-       
+
 
         // initialize W matrix: Wf = 0, W: m by n matrix
         int m = static_cast<int>(normal_points_0.size());  // number of rows
         int n = 9;  // according to notes, number of columns = 9
         Matrix W(m, n);
-        
+
         // define u, v for normalpoints_0, u_, v_ for normalpoints_1
         double u{}, v{}, u_{}, v_{};
         for (int i = 0; i != normal_points_0.size(); ++i)
         {
-            u  = normal_points_0[i].x(); v  = normal_points_0[i].y();
+            u = normal_points_0[i].x(); v = normal_points_0[i].y();
             u_ = normal_points_1[i].x(); v_ = normal_points_1[i].y();
             W.set_row(i, { u * u_, v * u_, u_, u * v_, v * v_, v_, u, v, 1 });
         }
-        
+
 
         // Now matrix W (constructed by normalized points) is constructed ----------------------------
 
 
         // solve W using SVD
-		Matrix U(m, m, 0.0);   // initialized with 0s
-		Matrix S(m, n, 0.0);   // initialized with 0s
-		Matrix V(n, n, 0.0);   // initialized with 0s
-	    svd_decompose(W, U, S, V);
+        Matrix U(m, m, 0.0);   // initialized with 0s
+        Matrix S(m, n, 0.0);   // initialized with 0s
+        Matrix V(n, n, 0.0);   // initialized with 0s
+        svd_decompose(W, U, S, V);
 
         // Form F using the last column of V
         Vector vlc = V.get_column(V.cols() - 1);  // get the last column of V
@@ -263,13 +263,13 @@ namespace GEO1016_A2 {
  *      and the recovered relative pose must be written to R and t.
  */
 bool Triangulation::triangulation(
-        double fx, double fy,     /// input: the focal lengths (same for both cameras)
-        double cx, double cy,     /// input: the principal point (same for both cameras)
-        const std::vector<Vector2D> &points_0,  /// input: 2D image points in the 1st image.
-        const std::vector<Vector2D> &points_1,  /// input: 2D image points in the 2nd image.
-        std::vector<Vector3D> &points_3d,       /// output: reconstructed 3D points
-        Matrix33 &R,   /// output: 3 by 3 matrix, which is the recovered rotation of the 2nd camera
-        Vector3D &t    /// output: 3D vector, which is the recovered translation of the 2nd camera
+    double fx, double fy,     /// input: the focal lengths (same for both cameras)
+    double cx, double cy,     /// input: the principal point (same for both cameras)
+    const std::vector<Vector2D>& points_0,  /// input: 2D image points in the 1st image.
+    const std::vector<Vector2D>& points_1,  /// input: 2D image points in the 2nd image.
+    std::vector<Vector3D>& points_3d,       /// output: reconstructed 3D points
+    Matrix33& R,   /// output: 3 by 3 matrix, which is the recovered rotation of the 2nd camera
+    Vector3D& t    /// output: 3D vector, which is the recovered translation of the 2nd camera
 ) const
 {
     /// NOTE: there might be multiple workflows for reconstructing 3D geometry from corresponding image points.
@@ -279,22 +279,22 @@ bool Triangulation::triangulation(
     ///       triangulation(), or put them in one or multiple separate files.
 
     std::cout << "\nTODO: I am going to implement the triangulation() function in the following file:" << std::endl
-              << "\t    - triangulation_method.cpp\n\n";
+        << "\t    - triangulation_method.cpp\n\n";
 
     std::cout << "[Liangliang]:\n"
-                 "\tFeel free to use any provided data structures and functions. For your convenience, the\n"
-                 "\tfollowing three files implement basic linear algebra data structures and operations:\n"
-                 "\t    - Triangulation/matrix.h  Matrices of arbitrary dimensions and related functions.\n"
-                 "\t    - Triangulation/vector.h  Vectors of arbitrary dimensions and related functions.\n"
-                 "\t    - Triangulation/matrix_algo.h  Determinant, inverse, SVD, linear least-squares...\n"
-                 "\tPlease refer to the above files for a complete list of useful functions and their usage.\n\n"
-                 "\tIf you choose to implement the non-linear method for triangulation (optional task). Please\n"
-                 "\trefer to 'Tutorial_NonlinearLeastSquares/main.cpp' for an example and some explanations.\n\n"
-                 "\tIn your final submission, please\n"
-                 "\t    - delete ALL unrelated test or debug code and avoid unnecessary output.\n"
-                 "\t    - include all the source code (and please do NOT modify the structure of the directories).\n"
-                 "\t    - do NOT include the 'build' directory (which contains the intermediate files in a build step).\n"
-                 "\t    - make sure your code compiles and can reproduce your results without ANY modification.\n\n" << std::flush;
+        "\tFeel free to use any provided data structures and functions. For your convenience, the\n"
+        "\tfollowing three files implement basic linear algebra data structures and operations:\n"
+        "\t    - Triangulation/matrix.h  Matrices of arbitrary dimensions and related functions.\n"
+        "\t    - Triangulation/vector.h  Vectors of arbitrary dimensions and related functions.\n"
+        "\t    - Triangulation/matrix_algo.h  Determinant, inverse, SVD, linear least-squares...\n"
+        "\tPlease refer to the above files for a complete list of useful functions and their usage.\n\n"
+        "\tIf you choose to implement the non-linear method for triangulation (optional task). Please\n"
+        "\trefer to 'Tutorial_NonlinearLeastSquares/main.cpp' for an example and some explanations.\n\n"
+        "\tIn your final submission, please\n"
+        "\t    - delete ALL unrelated test or debug code and avoid unnecessary output.\n"
+        "\t    - include all the source code (and please do NOT modify the structure of the directories).\n"
+        "\t    - do NOT include the 'build' directory (which contains the intermediate files in a build step).\n"
+        "\t    - make sure your code compiles and can reproduce your results without ANY modification.\n\n" << std::flush;
 
     /// Below are a few examples showing some useful data structures and APIs.
 
@@ -315,13 +315,13 @@ bool Triangulation::triangulation(
 
     /// define and initialize a 3 by 3 matrix
     Matrix33 T1(1.1, 2.2, 3.3,
-               0, 2.2, 3.3,
-               0, 0, 1);
+        0, 2.2, 3.3,
+        0, 0, 1);
 
     /// define and initialize a 3 by 4 matrix
     Matrix34 M(1.1, 2.2, 3.3, 0,
-               0, 2.2, 3.3, 1,
-               0, 0, 1, 1);
+        0, 2.2, 3.3, 1,
+        0, 0, 1, 1);
 
     /// set first row by a vector
     M.set_row(0, Vector4D(1.1, 2.2, 3.3, 4.4));
@@ -332,7 +332,7 @@ bool Triangulation::triangulation(
     /// define a 15 by 9 matrix (and all elements initialized to 0.0)
     Matrix W(15, 9, 0.0);
     /// set the first row by a 9-dimensional vector
-    W.set_row(0, {0, 1, 2, 3, 4, 5, 6, 7, 8}); // {....} is equivalent to a std::vector<double>
+    W.set_row(0, { 0, 1, 2, 3, 4, 5, 6, 7, 8 }); // {....} is equivalent to a std::vector<double>
 
     /// get the number of rows.
     int num_rows = W.rows();
@@ -380,13 +380,13 @@ bool Triangulation::triangulation(
     }
     Matrix33 T = trans_0.first; Matrix33 T_ = trans_1.first;
     // get transform matrix --------------------------------------------------------------------------
-    
+
 
     // normalize points ------------------------------------------------------------------------------
     auto normal_points_0 = GEO1016_A2::NormalizePoints(points_0, T);
     auto normal_points_1 = GEO1016_A2::NormalizePoints(points_1, T_);
     // normalize points ------------------------------------------------------------------------------
-    
+
 
     // get fundamental matrix from Wf = 0, use SVD to solve W ----------------------------------------
     auto Funda = GEO1016_A2::getFundaFromSVD(normal_points_0, normal_points_1);
